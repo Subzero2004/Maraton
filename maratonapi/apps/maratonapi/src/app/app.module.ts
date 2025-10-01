6d90657000089b19d98046f1385d0bc24ba4153b
Subzero2004 <88676361+Subzero2004@users.noreply.github.com>
2025-10-01 17:57:46 -0300

    Connection PostgreSQL

--- maratonapi/apps/maratonapi/src/app/app.module.ts
+++ maratonapi/apps/maratonapi/src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AtletaModule } from './atleta/atleta.module';
+import { CiudadModule } from './ciudad/ciudad.module';
 
 @Module({
-  imports: [],
+  imports:
+  [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'maraton_user',
+      password: 'maraton_password',
+      database: 'db_maraton',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AtletaModule,
+    CiudadModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
